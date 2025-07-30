8819b06116bea8b8ab5374226ba36c143ccfb79b
bntmzu <kuranova988@gmail.com>
2025-07-31 01:10:30 +0200

    chore(setup): add project entrypoint, Docker, config and root setup files

--- src/odme_anomalies/main.py
+++ src/odme_anomalies/main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
+from odme_anomalies.api.v1.router import router
 
-app = FastAPI(title="ODME - mythological anomalies")
+app = FastAPI(
+    title="ODME – Mythological Anomalies",
+    description="Systém pro hlášení a sledování mytologických projevů",
+    version="1.0.0",
+)
 
 
-@app.get("/")
+@app.get("/", tags=["Healthcheck"])
 async def root():
     return {"message": "ODME API is running"}
 
+
+app.include_router(router, prefix="/v1")
